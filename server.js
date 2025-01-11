0dc0a2c48fea346e1f7475824882c30c9730bf09
Gonzalo Coronado <jgcoronadocueva@outlook.com>
2025-01-10 23:00:28 -0500

    Assignment 1 - Publish API to the web

---
+++ server.js
@@ -0,0 +1,29 @@
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+
+/* ***********************
+ * Require Statements
+ *************************/
+const express = require('express');
+const app = express();
+
+// Route to return someone's name
+app.get('/', (req, res) => {
+    res.send('John Doe');
+});
+
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+const port = process.env.PORT
+const host = process.env.HOST
+
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+app.listen(port, () => {
+    console.log(`Server is running on ${host}:${port}`);
+});
